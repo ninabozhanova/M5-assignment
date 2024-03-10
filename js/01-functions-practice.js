34e9a88698e7e0a22a94913134c533d119d8eae4
ninabozhanova <nbozhanova@gmail.com>
2024-03-09 19:29:25 -0800

    Done with #1

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -1,9 +1,30 @@
 //STEP 1
+function halfNumber(inputNumber) {
+    let halfNum = inputNumber / 2
+    console.log(`Half of ${inputNumber} is ${halfNum}.`)
+    return halfNum
+}
 
 //STEP 2
+function squareNumber(inputNumber) {
+    let squareNum = inputNumber * inputNumber
+    console.log(`The result of squaring the number ${inputNumber} is ${squareNum}.`)
+    return squareNum
+}
 
 //STEP 3
+function percentOf(num1, num2) {
+    let calcResult = num1 / num2 * 100
+    console.log(`${num1} is ${calcResult.toFixed(1)}% of ${num2}.`)
+    return calcResult
+}
 
 //STEP 4
+function findModulus(num1, num2) {
+    let modulusResult = num1 % num2
+    console.log(` ${num1} mod ${num2} is ${modulusResult}.`)
+    return modulusResult
+}
 
 //STEP 5
+// I dindn't find instructions for STEP 5...
